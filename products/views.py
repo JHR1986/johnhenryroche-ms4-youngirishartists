f600918f4a1d49c57bf4108e08fd311787ea6230
JHR1986 <jhroche1986@gmail.com>
2021-07-24 12:15:36 +0000

    Resolve issue with browser not opening through rewriting urls.py text

--- products/views.py
+++ products/views.py
@@ -3,9 +3,8 @@ from .models import Product
 
 # Create your views here.
 
-
 def all_products(request):
-    """ A view to show all artworks, including sorting and search queries """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
 
@@ -22,7 +21,7 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': product,
+        'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
