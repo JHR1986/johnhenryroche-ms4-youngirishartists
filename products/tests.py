282d4ce7b1c1852a7ec95d6dbbb0e6dce3de0f01
JHR1986 <jhroche1986@gmail.com>
2021-07-27 09:25:08 +0000

    Create View Tests for Products - Test Driven Development

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Product
+
 
 # Create your tests here.
+def test_get_products(self):
+    response = self.client.get('/products/')
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'products/products.html')
+
+
+def test_get_products_detail(self):
+    response = self.client.get('/products/2')
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'products/product_detail.html')
