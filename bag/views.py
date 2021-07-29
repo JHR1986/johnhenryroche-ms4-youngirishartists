b2172dc28b7ece224238fdac22cb72f8d529f871
JHR1986 <jhroche1986@gmail.com>
2021-07-29 09:09:26 +0000

    Test that toast success message is working - Test Driven Development

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
 
-from products.models import Product 
+from products.models import Product
 
 # Create your views here.
 
@@ -15,7 +15,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    product = Product.objects.get(pk=item)
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
