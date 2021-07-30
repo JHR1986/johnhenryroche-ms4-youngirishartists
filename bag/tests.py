7bac9f5e63f2b5e4a14f9713fc5f6f225c1116e0
JHR1986 <jhroche1986@gmail.com>
2021-07-30 08:52:17 +0000

    Test that bag view is working correctly - Test Driven Development

--- bag/tests.py
+++ bag/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+def test_get_bag(self):
+    response = self.client.get('/bag/')
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'bag/bag.html')
