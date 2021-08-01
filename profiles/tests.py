c16d39fb69906959b573af1e7592b3a2494a9f25
JHR1986 <jhroche1986@gmail.com>
2021-08-01 10:44:33 +0000

    Use tests.py to ensure profile view is working - Test Driven Development

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_get_profile_page(self):
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profiles/profile.html')
