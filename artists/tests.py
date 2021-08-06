4f7edb3dbc811ae6cbfb201c5666952fdd8adb61
JHR1986 <jhroche1986@gmail.com>
2021-08-06 10:12:34 +0000

    Check view is correct for Artists page - Test Driven Development

--- artists/tests.py
+++ artists/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+# Test Run for Artists View.
+class TestViews(TestCase):
+
+    def test_get_artists(self):
+        response = self.client.get('/artists/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'artists/artists.html')
